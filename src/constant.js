0c28bfc156edf8a610f07e7af53cd17b2357b7c5
AdityaPunjcreator <adityajaiswal554@gmail.com>
2024-02-23 19:41:41 +0530

    baseURLchange

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api/v1/reactogram/";
+const API_BASE_URL =
+  "https://reactogrambackend-1.onrender.com/api/v1/reactogram/";
 
 export default API_BASE_URL;
